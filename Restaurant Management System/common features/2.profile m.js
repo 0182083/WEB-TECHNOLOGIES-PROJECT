761c53b04767fc9b2bcdda62cf2895225158d404
Saafayat Hasan <hedoyhasan111@gmail.com>
2025-05-20 20:53:35 +0600

    add new file

--- Restaurant Management System/common features/2.profile m.js
+++
@@ -1,9 +0,0 @@
-function previewImage(event) {
-  const reader = new FileReader();
-  reader.onload = function () {
-    const avatar = document.getElementById('avatarPreview');
-    avatar.src = reader.result;
-  };
-  reader.readAsDataURL(event.target.files[0]);
-}
- 
